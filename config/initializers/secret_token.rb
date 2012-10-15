0309995bace41f79e5a52a31d7d1222594cabfdb
Bruno Ghisi <bruno.ghisi@resultadosdigitais.com.br>
2012-10-15 15:21:53 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeccomTestDemo::Application.config.secret_token = 'a45dbeca7aa23e2432c0291d18ce9f05f8c6b967a771d545befe3fee803d606106f64b2cdd0810ddb4540ff1de841a7c33c26275d11f7fbe3e5a3ec58f72fc1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SeccomTestDemo::Application.config.session_store :cookie_store, key: '_seccom-test-demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SeccomTestDemo::Application.config.session_store :active_record_store
